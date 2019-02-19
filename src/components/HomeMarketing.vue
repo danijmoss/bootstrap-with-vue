1397bab0e09e43847c7e81605b8c448587592a7d
Danielle Moss <dani.code.camp@gmail.com>
2019-02-18 20:14:33 -0500

    started to add images to the site

--- src/components/HomeMarketing.vue
+++ src/components/HomeMarketing.vue
@@ -2,69 +2,26 @@
   <!-- Three columns of text below the carousel -->
   <div class="row">
     <div class="col-lg-4">
-      <svg
-        class="bd-placeholder-img rounded-circle"
-        width="140"
-        height="140"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMidYMid slice"
-        focusable="false"
-        role="img"
-        aria-label="Placeholder: 140x140"
-      >
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="#777"></rect>
-        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-      </svg>
-      <h2>Heading</h2>
-      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-      <p>
-        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
-      </p>
+      <img src="../assets/pixelfest.png" class="rounded pb-3" alt="PixelFest Icon">
+
+      <h2>Created for Pixelfest Developer Day 2018</h2>
     </div>
     <!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <svg
-        class="bd-placeholder-img rounded-circle"
-        width="140"
-        height="140"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMidYMid slice"
-        focusable="false"
-        role="img"
-        aria-label="Placeholder: 140x140"
-      >
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="#777"></rect>
-        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-      </svg>
-      <h2>Heading</h2>
-      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-      <p>
-        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
-      </p>
+      <img src="../assets/quicksilver.png" class="rounded pb-3" alt="Illustration of Danielle Moss">
+
+      <h2>Created by Danielle Moss</h2>
     </div>
     <!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <svg
-        class="bd-placeholder-img rounded-circle"
-        width="140"
-        height="140"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMidYMid slice"
-        focusable="false"
-        role="img"
-        aria-label="Placeholder: 140x140"
+      <img
+        src="../assets/unity-icon.jpg"
+        class="rounded pb-3"
+        style="max-width: 180px;"
+        alt="Unity3D Logo"
       >
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="#777"></rect>
-        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-      </svg>
-      <h2>Heading</h2>
-      <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p>
-        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
-      </p>
+
+      <h2>Made with Unity</h2>
     </div>
     <!-- /.col-lg-4 -->
   </div>
