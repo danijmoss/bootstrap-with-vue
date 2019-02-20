90abd6675786452cabefc884b1daddbcd6cfcfe4
Danielle Moss <dani.code.camp@gmail.com>
2019-02-19 20:17:24 -0500

    Completed added images to the site. Added in my own text to the site

--- src/components/HomeFeaturette.vue
+++ src/components/HomeFeaturette.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
     <hr class="featurette-divider">
+    <h2 class="featurette-heading text-center">What are you waiting for! Get to that party!</h2>
+    <hr class="featurette-divider">
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">
-          First featurette heading.
-          <span class="text-muted">It’ll blow your mind.</span>
+        <h2 class="featurette-heading">Collect the red balloons for points.
+          <br>
+          <br>
+          <span class="text-muted py-4">Try to beat your top score!</span>
         </h2>
-        <p
-          class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-5">
-        <svg
-          class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-          width="500"
-          height="500"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-          role="img"
-          aria-label="Placeholder: 500x500"
+        <img
+          src="../assets/balloon-points.png"
+          alt="Collect balloons for points! Try to beat your high score!"
+          class="rounded"
         >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#eee"></rect>
-          <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-        </svg>
       </div>
     </div>
 
@@ -34,29 +25,18 @@
 
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">
-          Oh yeah, it’s that good.
-          <span class="text-muted">See for yourself.</span>
+        <h2 class="featurette-heading text-right">Avoid obstacles...
+          <br>
+          <br>
+          <span class="text-muted text-right">or the game will restart!</span>
         </h2>
-        <p
-          class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg
-          class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-          width="500"
-          height="500"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-          role="img"
-          aria-label="Placeholder: 500x500"
+        <img
+          src="../assets/avoid-obstacles.png"
+          alt="Avoid obstalces on the game will restart!"
+          class="rounded float-left"
         >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#eee"></rect>
-          <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-        </svg>
       </div>
     </div>
 
@@ -64,29 +44,18 @@
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">
-          And lastly, this one.
-          <span class="text-muted">Checkmate.</span>
+        <h2 class="featurette-heading">Collect items along the way.
+          <br>
+          <br>
+          <span class="text-muted">Don't forget your party hat!</span>
         </h2>
-        <p
-          class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-5">
-        <svg
-          class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-          width="500"
-          height="500"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-          role="img"
-          aria-label="Placeholder: 500x500"
+        <img
+          src="../assets/collect-items02.png"
+          alt="Collect items along the way. Don't forget your party hat!"
+          class="rounded"
         >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#eee"></rect>
-          <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-        </svg>
       </div>
     </div>
 
--- src/components/Shared/SiteFooter.vue
+++ src/components/Shared/SiteFooter.vue
@@ -3,11 +3,7 @@
     <p class="float-right">
       <a href="#">Back to top</a>
     </p>
-    <p>
-      &copy; 2017-2019 Company, Inc. &middot;
-      <a href="#">Privacy</a> &middot;
-      <a href="#">Terms</a>
-    </p>
+    <p>&copy; 2018 Danielle J. Moss</p>
   </footer>
 </template>
 
